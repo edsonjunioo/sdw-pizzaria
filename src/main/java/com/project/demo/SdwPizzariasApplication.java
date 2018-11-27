8c6bc395fbd8e9551dadfac01f7565892e16ea18
edsonjunioo <edsonjunio.sousa@gmail.com>
2018-11-26 23:50:58 -0200

    first commit

---
+++ src/main/java/com/project/demo/SdwPizzariasApplication.java
@@ -0,0 +1,12 @@
+package com.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdwPizzariasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdwPizzariasApplication.class, args);
+    }
+}
